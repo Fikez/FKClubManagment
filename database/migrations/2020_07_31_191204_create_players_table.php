e7cb79e779e65834feefe67dbe288080d5cd6a0e
Nikola <cirkoyut@gmail.com>
2020-07-31 21:34:13 +0200

    Initial push && Club and Players base code

---
+++ database/migrations/2020_07_31_191204_create_players_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->date("birthday");
+            $table->string("country");
+            $table->string("position");
+            $table->integer("height");
+            $table->date("date_joined");
+            $table->string("career")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
